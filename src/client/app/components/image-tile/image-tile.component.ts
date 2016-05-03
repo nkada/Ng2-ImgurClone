aee603a3117422f898a3f055de350fa4e103aa66
Andrew Durber <aidurber@gmail.com>
2016-05-03 15:00:34 +0100

    Load thumbnail instead of direct link
    Performance

--- src/client/app/components/image-tile/image-tile.component.ts
+++ src/client/app/components/image-tile/image-tile.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input } from 'angular2/core';
+import {Component, Input, OnInit} from 'angular2/core';
 import { Router, RouteParams } from 'angular2/router';
 import {Image} from '../../image';
 
@@ -19,6 +19,14 @@ export class ImageTileComponent {
     constructor(
         private _router: Router
     ) {
+
+    }
+    public thumbnail: string;
+
+    ngOnInit(){
+      console.log(this.image);
+      var base = "http://i.imgur.com/"
+      this.thumbnail = `${base}${this.image.id}t.jpg`;
     }
 
     goToImageDetail(image: Image) {
