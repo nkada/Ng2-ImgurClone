e24607bcf8da56f2f09032e02d1e9017dc06f73d
Andrew Durber <aidurber@gmail.com>
2016-05-03 13:19:43 +0100

    Added loading spinner

--- src/client/app/components/category-images/category-images.component.ts
+++ src/client/app/components/category-images/category-images.component.ts
@@ -7,12 +7,12 @@ import {Image} from '../../image';
 import {Category} from '../../category';
 import { ImagesService } from '../../services/images.service';
 import {ImageTileComponent} from '../image-tile/image-tile.component';
-
+import {SpinnerComponent} from '../spinner/spinner.component';
 
 @Component({
     moduleId: __moduleName,
     selector: 'images',
-    directives: [ImageTileComponent],
+    directives: [ImageTileComponent, SpinnerComponent],
     providers: [provide(ImagesService, { useClass: ImagesService }), HTTP_PROVIDERS],
     templateUrl: 'category-images.component.html',
     styleUrls: ['category-images.component.css'],
@@ -20,6 +20,7 @@ import {ImageTileComponent} from '../image-tile/image-tile.component';
 
 export class CategoryImagesComponent implements OnInit {
     images: Image[] = [];
+    public isRequesting: boolean;
     topic: string;
     constructor(
         private _imagesService: ImagesService
@@ -29,12 +30,18 @@ export class CategoryImagesComponent implements OnInit {
 
     ngOnInit() {
         let id = this._routeParams.get("id");
+        this.isRequesting = true;
         this._imagesService.getImagesByCategory(id)
             .then((result) => {
                 this.images = result.data.filter((image) => {
                   this.topic = image.topic;
                     return !image.is_album;
                 })
+
+                this.stopRefreshing();
             });
+    }
+    private stopRefreshing() {
+        this.isRequesting = false;
     }
 }
