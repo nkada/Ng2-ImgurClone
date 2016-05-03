81737c40b7431bc1637eaf7e73bc2ffa79ffddf9
Andrew Durber <aidurber@gmail.com>
2016-05-03 15:01:44 +0100

    Pagination and performance

---
+++ app/page.js
@@ -0,0 +1,1 @@
+System.register([],function(exports_1,context_1){"use strict";var __moduleName=context_1&&context_1.id;var Page;return{setters:[],execute:function(){Page=function(){function Page(number,isActive){this.number=number;this.isActive=isActive}return Page}();exports_1("Page",Page)}}});
