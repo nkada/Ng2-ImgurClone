5811cdc756ac08b76429dc7b48853c97d51e423e
Andrew Durber <aidurber@gmail.com>
2016-05-03 15:01:09 +0100

    Get images by category based on pagination

--- src/client/app/components/category-images/category-images.component.ts
+++ src/client/app/components/category-images/category-images.component.ts
@@ -1,47 +1,64 @@
-import {Component, OnInit, provide} from 'angular2/core';
+import {Component, OnInit, provide, AfterViewInit, ViewChild, ChangeDetectorRef} from 'angular2/core';
 import { Router, RouteParams } from 'angular2/router';
 import {Http, Response, Headers, HTTP_BINDINGS, HTTP_PROVIDERS} from 'angular2/http';
 import 'rxjs/Rx';
 
 import {Image} from '../../image';
+import {Page} from '../../page';
 import {Category} from '../../category';
 import { ImagesService } from '../../services/images.service';
 import {ImageTileComponent} from '../image-tile/image-tile.component';
 import {SpinnerComponent} from '../spinner/spinner.component';
+import {PaginationComponent} from '../pagination/pagination.component';
 
 @Component({
     moduleId: __moduleName,
     selector: 'images',
-    directives: [ImageTileComponent, SpinnerComponent],
+    directives: [ImageTileComponent, SpinnerComponent, PaginationComponent],
     providers: [provide(ImagesService, { useClass: ImagesService }), HTTP_PROVIDERS],
     templateUrl: 'category-images.component.html',
     styleUrls: ['category-images.component.css'],
 })
 
-export class CategoryImagesComponent implements OnInit {
+export class CategoryImagesComponent  {
     images: Image[] = [];
     public isRequesting: boolean;
     topic: string;
-    public page: number = 1;
+    public page: Page;
     constructor(
         private _imagesService: ImagesService
         , private _router: Router
         , private _routeParams: RouteParams
+        , private _changeDetectionRef: ChangeDetectorRef
     ) { }
 
-    ngOnInit() {
-        let id = this._routeParams.get("id");
+    @ViewChild(PaginationComponent)
+    private _paginationComponent: PaginationComponent;
+    ngAfterViewInit() {
+        this.page = this._paginationComponent.currentPage;
+        this.fetchImages(this.page.number);
+        this._changeDetectionRef.detectChanges();
+    }
+
+  onPageChange(page:boolean){
+    console.log("Changed");
+    this.fetchImages(this._paginationComponent.currentPage.number);
+  }
+    fetchImages(pageNumber: number) {
         this.isRequesting = true;
-        this._imagesService.getImagesByCategory(id, {page: this.page})
+        let id = this._routeParams.get("id");
+
+        this._imagesService.getImagesByCategory(id, { page: pageNumber })
             .then((result) => {
                 this.images = result.data.filter((image) => {
-                  this.topic = image.topic;
+                    this.topic = image.topic;
                     return !image.is_album;
                 })
 
                 this.stopRefreshing();
             });
     }
+
     private stopRefreshing() {
         this.isRequesting = false;
     }
